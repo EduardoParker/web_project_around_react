01950d3631596bc183cef156f161b406e7baa6c2
EduardoParker <ingeparker555@gmail.com>
2024-09-19 18:37:57 -0600

    se trabaja en los popups

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "../blocks/footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <p className="footer__copyright">&#169; 2024 Eduardo Parker</p>
+      </footer>
+    </>
+  );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,187 @@
+import React from "react";
+import "../blocks/profile.css";
+import profileImage from "../images/profile.jpg";
+import buttonProfileUpdate from "../images/Edit_Button.svg";
+import addButtonImage from "../images/Add_Button.svg";
+import PopupWithForm from "./PopupWithForm";
+import ImagePopup from "./ImagePopup";
+
+export default function Main({
+  onEditProfileClick,
+  onAddPlaceClick,
+  onEditAvatarClick,
+  isOpenProfile,
+  isOpenAddPlace,
+  isOpenAvatar,
+  handleClosePopup,
+}) {
+  const handleEditProfileClick = () => {
+    onEditProfileClick();
+  };
+
+  const handleEditAddPlaceClick = () => {
+    onAddPlaceClick();
+  };
+
+  const handleEditAvatarClick = () => {
+    onEditAvatarClick();
+  };
+
+  /*const handleEditProfileClick = () => {
+    document.querySelector(".popup_profile").classList.add("popup_opened");
+    //popup.classList.add("popup_opened ");
+  };
+
+  const handleEditAvatarClick = () => {
+    document.querySelector(".popup_avatar").classList.add("popup_opened");
+    //popup.classList.add("popup_opened ");
+  };
+
+  const handleEditAddPlaceClick = () => {
+    document.querySelector(".popup_form_image").classList.add("popup_opened");
+    //popup.classList.add("popup_opened ");
+  };
+*/
+
+  return (
+    <>
+      <PopupWithForm
+        name="profile"
+        title="Editar perfil"
+        isOpen={isOpenProfile}
+        //onClose={isOpenProfile}
+        handleClosePopup={handleClosePopup}
+      >
+        <fieldset className="form__input">
+          <input
+            type="text"
+            className="form__item form__item_name"
+            id="name"
+            name="name"
+            placeholder="Nombre"
+            required
+            //minlength="2"
+            //maxlength="40"
+          />
+          <span className="form__error name-error"></span>
+          <input
+            type="text"
+            className="form__item form__item_about-me"
+            id="about-me"
+            name="about"
+            placeholder="Acerca de mi"
+            required
+            //minlength="2"
+            //maxlength="200"
+          />
+          <span className="form__error about-error"></span>
+        </fieldset>
+        <button
+          className="form__button form__button_submit"
+          type="submit"
+          id="update_profile"
+        >
+          Guardar
+        </button>
+      </PopupWithForm>
+      <PopupWithForm
+        name="form_image"
+        title="Nuevo Lugar"
+        isOpen={isOpenAddPlace}
+      >
+        <fieldset className="form__input">
+          <input
+            type="text"
+            className="form__item form__item_title"
+            id="title"
+            name="name"
+            placeholder="Titulo"
+            required
+            //minlength="2"
+            //maxlength="30"
+          />
+          <span className="form__error name-error"></span>
+          <input
+            type="url"
+            className="form__item form__item_link"
+            id="link"
+            name="link"
+            placeholder="Enlace a la imagen"
+            required
+          />
+          <span className="form__error link-error"></span>
+        </fieldset>
+        <button
+          className="form__button form__button_submit form__button_submit_image"
+          type="submit"
+          id="create_image"
+        >
+          Crear
+        </button>
+      </PopupWithForm>
+      <PopupWithForm
+        name="avatar"
+        title="Cambiar foto de perfil"
+        isOpen={isOpenAvatar}
+      >
+        <fieldset className="form__input">
+          <input
+            type="url"
+            className="form__item form__item_avatar-link"
+            id="avatar"
+            name="avatar"
+            placeholder="Enlace a la imagen del avatar"
+            required
+          />
+          <span className="form__error avatar-error"></span>
+        </fieldset>
+        <button
+          className="form__button form__button_submit"
+          type="submit"
+          id="update_avatar"
+        >
+          Guardar
+        </button>
+      </PopupWithForm>
+      <ImagePopup />
+
+      {
+        //empieza apartado de profile main
+      }
+      <section className="profile">
+        <div className="profile__avatar" onClick={handleEditAvatarClick}>
+          <img
+            src={profileImage}
+            alt="imagen de perfil"
+            className="profile__image-avatar"
+          />
+          <div className="profile__avatar-container">
+            <img
+              src={buttonProfileUpdate}
+              alt="boton para editar avatar"
+              className="profile__update-button"
+            />
+          </div>
+        </div>
+        <div className="profile__info">
+          <div className="profile__info-name">
+            <h1 className="profile__name">Jacques Cousteau</h1>
+            <img
+              src={buttonProfileUpdate}
+              onClick={handleEditProfileClick}
+              alt="boton para editar perfil"
+              className="profile__edit-button button"
+            />
+          </div>
+          <h2 className="profile__info-aboutme">Explorador</h2>
+        </div>
+        <img
+          src={addButtonImage}
+          onClick={handleEditAddPlaceClick}
+          alt="boton para añadir contenido"
+          className="profile__add-button button"
+        />
+      </section>
+    </>
+  );
+}
